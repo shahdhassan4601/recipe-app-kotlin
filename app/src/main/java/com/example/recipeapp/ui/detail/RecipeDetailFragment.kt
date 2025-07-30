9404d82edeed0a3e5418fca1311e770f98584a59
Shahd Hassan <shahd.hassan.osh@gmail.com>
2025-07-30 10:37:03 +0300

    Solve problems after merge 1

--- app/src/main/java/com/example/recipeapp/ui/detail/RecipeDetailFragment.kt
+++ app/src/main/java/com/example/recipeapp/ui/detail/RecipeDetailFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recipeapp.R
 import com.example.recipeapp.databinding.FragmentRecipeDetailBinding
